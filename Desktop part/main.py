5ade0101d741cb4eafd5d44dd320b4c3c7ec42b9
josefmicak <71832264+josefmicak@users.noreply.github.com>
2021-07-14 16:57:37 +0200

    Changed script behavior after key press
    Right after the user presses a key, the script refreshes the web file in shorter intervals for several seconds

--- Desktop part/main.py
+++ Desktop part/main.py
@@ -5,6 +5,8 @@ from pynput.keyboard import Key, Controller
 link = "http://YOUR.WEBSITE/file.txt"
 oldValue = "temp"
 newValue = "temp"
+iterationsCounter = 0
+keyPressed = False
 
 while True:
     siteContent = requests.get(link)
@@ -16,8 +18,16 @@ while True:
         key = getattr(Key, siteContentSplit[1])
         keyboard.press(key)
         keyboard.release(key)
+        keyPressed = True
+        iterationsCounter = 0
         print('Key pressed: ', str(key))
 
     oldValue = siteContent.text
-    time.sleep(1)
-
+    if iterationsCounter >= 30:
+        keyPressed = False
+        iterationsCounter = 0
+    if keyPressed:
+        iterationsCounter += 1
+        time.sleep(0.2)
+    else:
+        time.sleep(1)
--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Allows the user to use the computer's keyboard from other devices. It's mainly m
 - Spacebar - pause/play
 - Left Arrow key - rewind
 - Right Arrow key - forward
+
 As of now, it's possible to remotely control the computer's keyboard from a website. It should be able to do this from more devices soon.
 ![Website](./Images/Website.png)
