bf5b9693ded0afcf62e21a00624f635a043fa8e7
josefmicak <71832264+josefmicak@users.noreply.github.com>
2021-08-31 16:38:56 +0200

    GUI desktop app created
    Created a GUI desktop app (using tkinter), which allows the user to change certain settings.

--- Desktop part/main.py
+++ Desktop part/main.py
@@ -1,13 +1,137 @@
+import tkinter as tk
+import webbrowser
+from tkinter import ttk
+import tkinter.font as font
+import tkinter.messagebox as messagebox
 import requests
-import time
 import random
 import json
+from datetime import datetime
 from pynput.keyboard import Key, Controller
 
+window = tk.Tk()
+menubar = tk.Menu(window)
+window.title("RemoteControlDesktop")
+window.resizable(False, False)
+
+scanBoolean = False
+fastScan = tk.IntVar()
 oldValue = "temp"
 newValue = "temp"
 iterationsCounter = 0
+fastScanAmount = 0
+fastScanFrequency = 0
+scanFrequency = 0
 keyPressed = False
+
+
+def default_values():
+    global fastScanAmount, fastScanFrequency, scanFrequency
+    fastScanAmount = 10
+    fastScanFrequency = 200
+    scanFrequency = 1000
+
+
+def switch_scan():
+    global scanBoolean, scanFrequency
+    if scanBoolean:
+        scanLabel.config(
+            text="Not scanning for keystrokes",
+            fg="red")
+        scanButton.config(
+            text="Start scanning",
+            fg="green"
+        )
+        scanFrequencyEntry.config(
+            state="normal"
+        )
+        scanFrequencySaveButton.config(
+            state="normal"
+        )
+    else:
+        window.after(scanFrequency, my_mainloop)
+        scanLabel.config(
+            text="Scanning for keystrokes",
+            fg="green")
+        scanButton.config(
+            text="Stop scanning",
+            fg="red"
+        )
+        scanFrequencyEntry.config(
+            state="disabled"
+        )
+        scanFrequencySaveButton.config(
+            state="disabled"
+        )
+    scanBoolean = not scanBoolean
+
+
+def my_mainloop():
+    global oldValue, newValue, iterationsCounter, keyPressed, fastScan, scanFrequency
+    callDelay = scanFrequency
+
+    if scanBoolean:
+        response = requests.get("http://testt.8u.cz/getKeyValues.php")
+        result = response.text
+        result = json.loads(result)
+        newValue = result[0].get("messageKey")
+
+        if newValue != oldValue and oldValue != "temp":
+            keyboard = Controller()
+            key = result[0].get("keyStroke")
+            if len(key) > 1:
+                key = getattr(Key, result[0].get("keyStroke"))
+            keyboard.press(key)
+            keyboard.release(key)
+            keyPressed = True
+            iterationsCounter = 0
+            now = datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            pressedKeysTV.insert(parent="", index=0, text="", values=(str(dt_string), str(key)))
+            send_response()
+
+        oldValue = result[0].get("messageKey")
+        if iterationsCounter >= fastScanAmount:
+            keyPressed = False
+            iterationsCounter = 0
+            fastScanLabel.config(
+                fg="black",
+                text="Fast scan offline"
+            )
+            fastScanCB.config(
+                state="normal"
+            )
+            fastScanAmountEntry.config(
+                state="normal"
+            )
+            fastScanFrequencyEntry.config(
+                state="normal"
+            )
+            fastScanSaveButton.config(
+                state="normal"
+            )
+        if keyPressed and fastScan.get() == True:
+            iterationsCounter += 1
+            callDelay = fastScanFrequency
+
+            fastScanLabel.config(
+                fg="green",
+                text=("Fast scan online (" + str(iterationsCounter) + " / " + str(fastScanAmount)) + ")"
+            )
+            if iterationsCounter == 1:
+                fastScanCB.config(
+                    state="disabled"
+                )
+                fastScanAmountEntry.config(
+                    state="disabled"
+                )
+                fastScanFrequencyEntry.config(
+                    state="disabled"
+                )
+                fastScanSaveButton.config(
+                    state="disabled"
+                )
+        window.after(callDelay, my_mainloop)
 
 
 def send_response():
@@ -16,31 +140,227 @@ def send_response():
     requests.post(url="http://testt.8u.cz/sendKeyStroke.php", data=data)
 
 
-while True:
-    response = requests.get("http://testt.8u.cz/getKeyValues.php")
-    result = response.text
-    result = json.loads(result)
-    print(result)
-    newValue = result[0].get("messageKey")
-
-    if newValue != oldValue and oldValue != "temp":
-        keyboard = Controller()
-        key = result[0].get("keyStroke")
-        if len(key) > 1:
-            key = getattr(Key, result[0].get("keyStroke"))
-        keyboard.press(key)
-        keyboard.release(key)
-        keyPressed = True
-        iterationsCounter = 0
-        print('Key pressed: ', str(key))
-        send_response()
-
-    oldValue = result[0].get("messageKey")
-    if iterationsCounter >= 30:
-        keyPressed = False
-        iterationsCounter = 0
-    if keyPressed:
-        iterationsCounter += 1
-        time.sleep(0.2)
+def fast_scan_switch():
+    if not fastScan.get():
+        fastScanLabel.config(
+            fg="red",
+            text="Fast scan disabled"
+        )
+        fastScanAmountEntry.config(
+            state="disabled"
+        )
+        fastScanFrequencyEntry.config(
+            state="disabled"
+        )
+        fastScanSaveButton.config(
+            state="disabled"
+        )
     else:
-        time.sleep(1)
+        fastScanLabel.config(
+            fg="black",
+            text="Fast scan offline"
+        )
+        fastScanAmountEntry.config(
+            state="normal"
+        )
+        fastScanFrequencyEntry.config(
+            state="normal"
+        )
+        fastScanSaveButton.config(
+            state="normal"
+        )
+
+
+def fast_scan_save():
+    global fastScanAmount, fastScanFrequency, scanFrequency
+    if int(fastScanFrequencyEntry.get()) > scanFrequency:
+        tk.messagebox.showerror("Error", ("Error: The fast scan frequency (" + (fastScanFrequencyEntry.get()) +
+                                          ") is higher than the scan frequency (" + str(scanFrequency) + ").\n" +
+                                          "Please input a lower value."))
+    else:
+        fastScanAmount = int(fastScanAmountEntry.get())
+        fastScanFrequency = int(fastScanFrequencyEntry.get())
+        tk.messagebox.showinfo("Success", "Fast scan values updated successfully")
+
+
+def fast_scan_about():
+    fastScanWindow = tk.Toplevel(window)
+    fastScanWindow.title("Fast scan")
+    tk.Label(fastScanWindow,
+             text="Immediately after a key press is detected, the frequency of scans can be temporarily increased.\n"
+                  "This is done because it's likely that the user will press multiple keys in a short time.\n"
+                  "The fast scan frequency as well as the amount of performed scans can be changed by the user.\n"
+                  "Default values: Amount 30, Frequency 200 ms",
+             justify=tk.LEFT).grid()
+
+
+def scan_frequency_save():
+    global scanFrequency, fastScanFrequency, fastScan
+    if int(scanFrequencyEntry.get()) < fastScanFrequency and (fastScan.get() == True):
+        tk.messagebox.showerror("Error", "Error: The scan frequency (" + scanFrequencyEntry.get() +
+                                ") is lower than the fast scan frequency (" + str(fastScanFrequency) + ").\n" +
+                                "Please either input a higher value or disable fast scan.")
+    else:
+        scanFrequency = int(scanFrequencyEntry.get())
+        tk.messagebox.showinfo("Success", "Scan frequency updated successfully")
+
+
+def show_help(event=None):
+    helpWindow = tk.Toplevel(window)
+    helpWindow.title("About")
+    tk.Label(helpWindow,
+             text="This program is part of a project called Remote Control.\n"
+                  "Its purpose is to make it possible to control your computer from remote devices.\n"
+                  "Further explanation, as well as the source code can be obtained at \n",
+             justify=tk.LEFT).grid()
+    linkLabel = tk.Label(helpWindow,
+                         text="https://github.com/josefmicak/remote-control",
+                         fg="blue",
+                         cursor="hand2",
+                         justify=tk.LEFT)
+    linkLabel.grid()
+    linkLabel.bind("<Button-1>", lambda e: callback("https://github.com/josefmicak/remote-control"))
+
+
+def callback(url):
+    webbrowser.open_new(url)
+
+
+def clear_pressed_keys():
+    pressedKeysTV.delete(*pressedKeysTV.get_children())
+
+
+default_values()
+
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu.add_command(label="Exit", command=window.destroy, accelerator="Alt+F4")
+menubar.add_cascade(label="File", menu=filemenu)
+
+helpmenu = tk.Menu(menubar, tearoff=0)
+helpmenu.add_command(label="About", command=show_help, accelerator="F1")
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+window.bind("<F1>", show_help)
+window.bind("<Alt-F4>", show_help)
+
+scanLF = tk.LabelFrame(text="Scan")
+scanLF.grid(row=0)
+
+scanLabel = tk.Label(
+    scanLF,
+    text="Not scanning for keystrokes",
+    fg="red",
+    font=("Courier", 30))
+scanLabel.grid(row=0, columnspan=10)
+
+scanButton = tk.Button(
+    scanLF,
+    text="Start scanning",
+    fg="green",
+    command=switch_scan
+)
+scanButton['font'] = font.Font(size=20)
+scanButton.grid(row=1, column=1, columnspan=3, sticky=tk.E)
+
+scanFrequencyLabel = tk.Label(
+    scanLF,
+    text="Scan frequency (in ms):")
+scanFrequencyLabel.grid(row=2, column=0, sticky=tk.W)
+
+scanFrequencyEntry = tk.Entry(
+    scanLF)
+scanFrequencyEntry.insert(tk.END, scanFrequency)
+scanFrequencyEntry.grid(row=2, column=1, sticky=tk.W)
+
+scanFrequencySaveButton = tk.Button(
+    scanLF,
+    text="Save",
+    command=scan_frequency_save
+)
+scanFrequencySaveButton.grid(row=2, column=1, columnspan=5)
+
+fastScanLF = tk.LabelFrame(text="Fast scan")
+fastScanLF.grid(row=1, column=0, sticky=tk.W)
+
+fastScanAboutButton = tk.Button(
+    fastScanLF,
+    text="What is fast scan?",
+    relief=tk.FLAT,
+    fg="blue",
+    cursor="hand2",
+    command=fast_scan_about
+)
+fastScanAboutButton.grid(row=0, column=0, sticky=tk.W)
+
+fastScanLabel = tk.Label(
+    fastScanLF,
+    text="Fast scan offline",
+    font=("Courier", 15))
+fastScanLabel.grid(row=1)
+
+fastScanCB = tk.Checkbutton(
+    fastScanLF,
+    text="Enable fast scan",
+    variable=fastScan,
+    command=fast_scan_switch)
+fastScanCB.select()
+fastScanCB.grid(row=2, column=0, sticky=tk.E)
+
+fastScanAmountLabel = tk.Label(
+    fastScanLF,
+    text="Amount of fast scans to perform:")
+fastScanAmountLabel.grid(row=3, column=0, sticky=tk.W)
+
+fastScanAmountEntry = tk.Entry(
+    fastScanLF)
+fastScanAmountEntry.insert(tk.END, fastScanAmount)
+fastScanAmountEntry.grid(row=3, column=1)
+
+fastScanFrequencyLabel = tk.Label(
+    fastScanLF,
+    text="Delay between scans (in ms):")
+fastScanFrequencyLabel.grid(row=4, column=0, sticky=tk.W)
+
+fastScanFrequencyEntry = tk.Entry(
+    fastScanLF)
+fastScanFrequencyEntry.insert(tk.END, fastScanFrequency)
+fastScanFrequencyEntry.grid(row=4, column=1)
+
+fastScanSaveButton = tk.Button(
+    fastScanLF,
+    text="Save",
+    command=fast_scan_save
+)
+fastScanSaveButton.grid(row=5, column=0, sticky=tk.E)
+
+pressedKeysLF = tk.LabelFrame(text="Pressed keys")
+pressedKeysLF.grid(row=1, column=0, sticky=tk.E)
+
+pressedKeysLabel = tk.Label(
+    pressedKeysLF,
+    text="Last 5 pressed keys:",
+    font=("Courier", 13))
+pressedKeysLabel.grid()
+
+pressedKeysTV = ttk.Treeview(
+    pressedKeysLF,
+    height=5
+)
+pressedKeysTV['columns'] = ('Time', 'Key')
+pressedKeysTV.column('#0', width=0, stretch=tk.NO)
+pressedKeysTV.column('Time', anchor=tk.CENTER, width=120)
+pressedKeysTV.column('Key', anchor=tk.CENTER, width=80)
+pressedKeysTV.heading('#0', text='', anchor=tk.CENTER)
+pressedKeysTV.heading('Time', text='Time', anchor=tk.CENTER)
+pressedKeysTV.heading('Key', text='Key', anchor=tk.CENTER)
+pressedKeysTV.grid(row=0)
+
+clearPressedKeysButton = tk.Button(
+    pressedKeysLF,
+    text="Clear",
+    command=clear_pressed_keys
+)
+clearPressedKeysButton.grid(row=1)
+
+window.config(menu=menubar)
+window.mainloop()
--- README.md
+++ README.md
@@ -1,29 +1,33 @@
 # Remote control
-Allows the user to use the computer's keyboard from other devices. It's mainly meant to be used for watching media, with 3 keys being able to be pressed this way so far:
+Allows the user to use the computer's keyboard from other devices. It's mainly meant to be used for watching media. Supports up to 6 keys, the 3 keys are not changeable:
 - Spacebar - pause/play
 - Left Arrow key - rewind
 - Right Arrow key - forward
+3 more keys can be modified by the user.
 
 There are also 2 indicator lights that determine whether the key press was successfully sent and delivered or not
 - First light (the left one) - determines whether the device successfully sent the key press
 - Second light (the right one) - determines whether the desktop app acknowledged the key press
 
-<p align="center"> 
-<img src="Images/Website.png">
-</p>
-
 ## Desktop part
 
 *Languages used: Python*
 
-Detects remote key presses and immediately performs them. For now there's no GUI, the key presses are printed to console.
+Detects remote key presses and immediately performs them. The user can change certain settings using a GUI application.
 
+<p align="center"> 
+<img src="Images/Desktop.png">
+</p>
 
 ## Website part
 
 *Languages used: HTML, CSS, JavaScript, PHP*
 
 Provides a way to perform remote key presses using a website. The .php files are required for the other parts of this project to work, the other files are optional.
+
+<p align="center"> 
+<img src="Images/Website.png">
+</p>
 
 
 ## Watch part
