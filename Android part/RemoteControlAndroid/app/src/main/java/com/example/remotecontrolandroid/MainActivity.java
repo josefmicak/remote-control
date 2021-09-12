745c8801d8f1e0c88e4da475c53111a8506f54a1
josefmicak <71832264+josefmicak@users.noreply.github.com>
2021-09-12 20:32:40 +0200

    Added indicator lights to Android part
    The Android part now contains indicator lights which let user know whether the key press was successful.

--- Android part/RemoteControlAndroid/app/src/main/java/com/example/remotecontrolandroid/MainActivity.java
+++ Android part/RemoteControlAndroid/app/src/main/java/com/example/remotecontrolandroid/MainActivity.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,9 +21,19 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
     private String loadedUsername;
     private String firstKey;
     private String secondKey;
@@ -32,11 +44,24 @@ public class MainActivity extends AppCompatActivity {
     private View secondKeyView;
     private TextView thirdKeyText;
     private View thirdKeyView;
+    private ImageView messageIndicatorLight;
+    private ImageView responseIndicatorLight;
+    int responseKey = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                controlIndicatorLight(messageIndicatorLight, R.color.red);
+                super.onReceivedError(view, errorCode, description, failingUrl);
+            }
+        });
+        messageIndicatorLight = findViewById(R.id.messageIndicatorLight);
+        responseIndicatorLight = findViewById(R.id.responseIndicatorLight);
         loadSettings();
     }
 
@@ -49,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
         switch (item.getItemId()) {
             case R.id.settings:
                 Intent settingsIntent = new Intent(this, Settings.class);
@@ -96,8 +120,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void sendText(View v)
-    {
+    public void sendText(View v) throws IOException, InterruptedException {
         if(loadedUsername.equals("Select your username"))
         {
             Toast.makeText(this, "Error: No username set! Please select your username in the settings.", Toast.LENGTH_LONG).show();
@@ -105,15 +128,85 @@ public class MainActivity extends AppCompatActivity {
         else
         {
             Random rand = new Random();
-            WebView webView = findViewById(R.id.webView);
             String url = "http://rc.8u.cz/sendKeyStroke.php";
             String type = "type=1";
             String username = "username=" + loadedUsername;
             String messageKey = "messageKey=" + rand.nextInt(10001);
             String keyStroke = "keyStroke=" + v.getTag().toString();
             String params = type +  "&" + username + "&" + messageKey + "&" + keyStroke;
+            controlIndicatorLight(messageIndicatorLight, R.color.limegreen);
             webView.postUrl(url,params.getBytes());
+            int oldResponseKey = checkResponseKey();
+            final int[] newResponseKey = {0};
+
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        checkResponseKey();
+                        newResponseKey[0] = checkResponseKey();
+                        if(oldResponseKey != newResponseKey[0])
+                        {
+                            controlIndicatorLight(responseIndicatorLight, R.color.limegreen);
+                        }
+                        else
+                        {
+                            controlIndicatorLight(responseIndicatorLight, R.color.red);
+                        }
+                    } catch (IOException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, 1500);
         }
+    }
+
+    public void controlIndicatorLight(ImageView lightId, int colorId)
+    {
+        lightId.setColorFilter(getResources().getColor(colorId));
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                lightId.setColorFilter(getResources().getColor(R.color.cyan));
+            }
+        }, 2000);
+    }
+
+    public int checkResponseKey() throws IOException, InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String getResultsUrl = "http://rc.8u.cz/getKeyValues.php?username=" + loadedUsername;
+                    URL url = new URL(getResultsUrl);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.connect();
+                    InputStream is = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                    String downloadedContent = "",data="";
+
+                    while ((data = reader.readLine()) != null){
+                        downloadedContent  += data + "\n";
+                    }
+                    JSONArray jsonArray = new JSONArray(downloadedContent);
+
+                    for(int i=0;i<jsonArray.length();i++)
+                    {
+                        JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                        responseKey = jsonObject1.optInt("responseKey");
+                    }
+
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        thread.join();
+        return responseKey;
     }
 
     public void onSettingsButton(View v)
@@ -128,7 +221,6 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onActivityResult(ActivityResult result) {
                     if (result.getResultCode() == Activity.RESULT_OK) {
-                        // Here, no request code
                         Intent data = result.getData();
                         loadSettings();
                     }
